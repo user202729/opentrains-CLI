5b2ab63770cc23853c7fcd0919f77ab0abd34070
user202729 <25191436+user202729@users.noreply.github.com>
2020-09-07 21:19:33 +0700

    Handle different contests correctly, better documentation

--- CLI.js
+++ CLI.js
@@ -18,13 +18,15 @@ process.on('unhandledRejection', err => { // for convenience in debugging
 
 // load stored username/password
 const storePath="/tmp/.opentrains-CLI-store"
-let username, password, contestId, Sid
+let username, password, SidOf
+let contestId // last used
 try{
-	({username, password, contestId, Sid}=JSON.parse(fs.readFileSync(storePath)))
+	({username, password, SidOf, contestId}=JSON.parse(fs.readFileSync(storePath)))
 }catch(e){
 	if(e.code!=="ENOENT")
 		throw e
 }
+if(!SidOf) SidOf={}
 
 function readline(query){ return new Promise(
 	//rl.resume()
@@ -35,6 +37,7 @@ function readline(query){ return new Promise(
 )}
 
 async function login(options){
+	// options: {username, contestid, password, url}
 	if(options.username) username=options.username
 	if(options.contestid) contestId=options.contestid
 	if(options.password) password=options.password
@@ -45,20 +48,20 @@ async function login(options){
 		password=await readline("Enter password: ")
 	}
 
-	if(!Sid){
+	if(!SidOf[contestId]){
 		saveFile()
 		const result=await axios.post(options.url, qs.stringify({
 			contest_id: contestId, role: 0, login: username, password,
 			locale_id: 0, // English
 			submit: "Log in" // also posted by form, but likely accidentally
 		}))
-		Sid=result.request.path.match(/\?SID=(.+?)&/)[1]
+		SidOf[contestId]=result.request.path.match(/\?SID=(.+?)&/)[1]
 	}
 	saveFile()
 }
 
 function saveFile(){
-	fs.writeFileSync(storePath, JSON.stringify({username, password, contestId, Sid}))
+	fs.writeFileSync(storePath, JSON.stringify({username, password, SidOf, contestId}))
 }
 
 async function checkSession(f){
@@ -74,54 +77,75 @@ async function checkSession(f){
 	return await f()
 }
 
-function printSubmissions(data){ // data: HTML string
+function parseSubmissions(data){ // data: HTML string
+	// might throw an error
+	// see printSubmissions for return format
 	const $=cheerio.load(data)
-	for(const row of $("table.b1 tr").toArray().slice(1))
-		console.log($(row).find("td").toArray().map(cell=>$(cell).text()))
+	if($("title").text()==="Operation completed with errors"){
+		fs.writeFileSync("/tmp/error_log.html", data)
+		throw Error($("pre").text().trim())
+	}
+	const rows=$("table.b1 tr").toArray()
+	if(rows.length===0){
+		fs.writeFileSync("/tmp/error_log.html", data)
+		throw Error("Invalid HTML")
+	}
+	return rows.slice(1).map(
+		row=>$(row).find("td").toArray().map(cell=>$(cell).text()).slice(0, -2)
+	)
+}
+
+function printSubmissions(data){ // data: HTML string
+	const result=parseSubmissions(data)
+	console.table(result.reverse().map(
+		([RunId, Time, Size, Problem, Language])=>({RunId, Time, Size, Problem, Language})
+	))
 }
 
 async function listAllCommand(options){
 	await login(options.parent)
-	const result=await checkSession(async ()=>await axios.get(`${options.parent.url}?SID=${Sid}&all_runs=1&action=140`))
+	const result=await checkSession(async ()=>await axios.get(`${options.parent.url}?SID=${SidOf[contestId]}&all_runs=1&action=140`))
 	// run id, time, size, problem, language, result, failed test, view source, view report
 	printSubmissions(result.data)
 }
 
-async function submitCommand(problemIndex, languageIndex, fileName, options){
-	await login(options.parent)
+async function submit(problemIndex, languageIndex, fileName, options){
+	// options: (same as function login)
+	/* languageIndex (at the time of writing)
+	2	gcc - GNU C 6.4.0
+	3	g++ - GNU C++ 8.2 (c++17)
+	5	javac - Java JDK 1.8
+	10	javac-32 - Java JDK (32 bit) 1.8
+	13	python - Python 2.7.5
+	23	python3 - Python3 3.6.1
+	63	pypy - Python (PyPy) 2.7.13
+	64	pypy3 - Python3 (PyPy) 3.5.3
+	66	kotlin - Kotlin (kotlinc-jvm 1.1.3-2)
+	91	dmd - Unidentified DMD
+	*/
+	await login(options)
 
 	if(!["2", "3", "5", "10", "13", "23", "63", "64", "66", "91"].includes(languageIndex))
 		throw Error("Invalid languageIndex: "+languageIndex)
 
-/*
-option><option value="2">gcc - GNU C 6.4.0<
-option><option value="3" selected="selected">g++ - GNU C++ 8.2 (c++17)<
-option><option value="5">javac - Java JDK 1.8<
-option><option value="10">javac-32 - Java JDK (32 bit) 1.8<
-option><option value="13">python - Python 2.7.5<
-option><option value="23">python3 - Python3 3.6.1<
-option><option value="63">pypy - Python (PyPy) 2.7.13<
-option><option value="64">pypy3 - Python3 (PyPy) 3.5.3<
-option><option value="66">kotlin - Kotlin (kotlinc-jvm 1.1.3-2)<
-option><option value="91">dmd - Unidentified DMD<
-*/
 
 	if(problemIndex!==parseInt(problemIndex).toString())
 		throw Error("Invalid problemIndex: "+problemIndex)
 
 	const form=new FormData() // multipart/form-data
-	form.append("SID", Sid)
+	form.append("SID", SidOf[contestId])
 	form.append("prob_id", problemIndex) // 11 for example
 	form.append("lang_id", languageIndex)
 	form.append("file", 
 		//fs.createReadStream(fileName),
 		fs.readFileSync(fileName),
 		path.basename(fileName))
-	// fiddling with the API is difficult.
-	// Something is wrong somewhere else too.
-	// The file name could be a reason.
 	form.append("action_40", "Send!") // perhaps unintentional?...
-	const result=await checkSession(async ()=>await axios.post(`${options.parent.url}`, form.getBuffer(), {headers: form.getHeaders()}))
+	return await checkSession(async ()=>await axios.post(`${options.url}`, form.getBuffer(), {headers: form.getHeaders()}))
+}
+
+async function submitCommand(problemIndex, languageIndex, fileName, options){
+	const result=submit(problemIndex, languageIndex, fileName, options.parent)
 	printSubmissions(result.data)
 }
 
@@ -130,9 +154,10 @@ program.version("0.0.0")
 	"http://opentrains.mipt.ru/~ejudge/team.cgi"
 	//"http://localhost/"
 )
-.option("-c, --contestid <contestid>", "contest id", "2708")
+.option("-c, --contestid <contestid>", "contest id (if not specified, the last contest ID will be used)")
 .option("-u, --username <username>", "username (login)", username)
-.option("-p, --password <password>", "password", password)
+.option("-p, --password <password>", "password (specify empty password for a prompt. "+
+	"If not specified, the last password will be used)", password)
 
 program.command("list_all")
 .alias("ls")
